857365f91ea662ecfda3c7ef081ddbc9e045f09e
zlDeath <matheushenrique_cunha@hotmail.com>
2020-04-02 18:26:42 -0300

    Update layout.blade.php

--- resources/views/site/layouts/layout.blade.php
+++ resources/views/site/layouts/layout.blade.php
@@ -16,7 +16,7 @@
    <link rel="icon" sizes="192x192" href="{{ asset('/assets/img/logoHM192x192.png') }}">
    <link rel="apple-touch-icon" href="{{ asset('/assets/img/logoHM192x192.png') }}">
    <link rel="shortcut icon" type="image/png href=" {{ asset('/assets/img/logoHM144x144.png') }}"">
-   <link rel="manifest" href="{{ asset('/assets/manifest/manifest.json') }}">
+   <!--<link rel="manifest" href="{{-- asset('/assets/manifest/manifest.json') --}}">-->
 
    <!--[ICON]-->
    <link rel="icon" href="{{ asset('/assets/img/icon.png') }}">
