52be5d7095727ba8a2c6b85ad0fe5c9f9511dbb1
zlDeath <matheushenrique_cunha@hotmail.com>
2020-04-02 18:34:50 -0300

    production

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,11 +1,1 @@
-$('.count').each(function() {
-    $(this).prop('Counter', 0).animate({
-        Counter: $(this).text()
-    }, {
-        duration: 7500,
-        easing: 'swing',
-        step: function(now) {
-            $(this).text(Math.ceil(now));
-        }
-    });
-});
+$(".count").each(function(){$(this).prop("Counter",0).animate({Counter:$(this).text()},{duration:7500,easing:"swing",step:function(t){$(this).text(Math.ceil(t))}})});
