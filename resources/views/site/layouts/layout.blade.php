a81d994af86d763ce325f109e3c84c632cb01b80
zlDeath <matheushenrique_cunha@hotmail.com>
2020-04-06 17:56:09 -0300

    Update layout.blade.php

--- resources/views/site/layouts/layout.blade.php
+++ resources/views/site/layouts/layout.blade.php
@@ -25,13 +25,13 @@
     <meta name="theme-color" content="#d80404">
     <meta name="apple-mobile-web-app-status-bar-style" content="#d80404">
     <meta name="msapplication-navbutton-color" content="#d80404">
-    <link rel="icon" sizes="192x192" href="{{ url('/assets/img/logo192x192.png') }}">
-    <link rel="apple-touch-icon" href="{{ url('/assets/img/logo192x192.png') }}">
-    <link rel="shortcut icon" type="image/png" href="{{ url('/assets/img/logo144x144.png') }}">
-    <link rel="manifest" href="{{ url('/manifest.json') }}">
+    <link rel="icon" sizes="192x192" href="/assets/img/logo192x192.png">
+    <link rel="apple-touch-icon" href="/assets/img/logo192x192.png">
+    <link rel="shortcut icon" type="image/png" href="/assets/img/logo144x144.png">
+    <link rel="manifest" href="/manifest.json">
 
     <!--[ICON]-->
-    <link rel="icon" href="{{ url('/assets/img/icon.png') }}">
+    <link rel="icon" href="/assets/img/icon.png">
 
     <!--[META:OG]-->
     <meta property="og:locale" content="pt-BR">
@@ -39,7 +39,7 @@
     <meta property="og:title" content="CoroVid">
     <meta property="og:site_name" content="CoroVid">
     <meta property="og:description" content="Visualize os casos de corona vírus da região!">
-    <meta property="og:image" content="{{ url('/assets/img/icon.png') }}">
+    <meta property="og:image" content="/assets/img/icon.png">
     <meta property="og:image:type" content="image/png">
     <meta property="og:image:width" content="144">
     <meta property="og:image:height" content="144">
@@ -47,7 +47,7 @@
     <!--[META:TWITTER]-->
     <meta name="twitter:title" content="CoroVid">
     <meta name="twitter:description" content="Visualize os casos de corona vírus da região!">
-    <meta name="twitter:image" content="{{ url('/assets/img/icon.png') }}">
+    <meta name="twitter:image" content="/assets/img/icon.png">
 
     <!--[META:AUTHOR]-->
     <meta property="article:author" content="M4theus.dev" href="https://M4theus.dev">
@@ -61,15 +61,15 @@
     <meta name="copyright" content="© CoroVid">
 
     <!--[LINK:Outros]-->
-    <link rel="stylesheet" href="{{ url('assets/css/sweetalert2-dark.css') }}">
-    <link rel="stylesheet" href="{{ url('assets/css/bootstrap/bootstrap.css') }}">
+    <link rel="stylesheet" href="assets/css/sweetalert2-dark.css">
+    <link rel="stylesheet" href="assets/css/bootstrap/bootstrap.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
-    <link rel="stylesheet" href="{{ url('assets/css/font-awesome-animation.min.css') }}">
-    <link rel="stylesheet" href="{{ url('assets/css/animate.min.css') }}">
-    <script src="{{ url('assets/js/sweetalert2.min.js') }}"></script>
+    <link rel="stylesheet" href="assets/css/font-awesome-animation.min.css">
+    <link rel="stylesheet" href="assets/css/animate.min.css">
+    <script src="assets/js/sweetalert2.min.js"></script>
 
     <!--[LINK:Styles]-->
-    <link href="{{ url('assets/css/style.css') }}" rel="stylesheet">
+    <link href="assets/css/style.css" rel="stylesheet">
 
 
 
@@ -85,7 +85,7 @@
             <nav class="navbar menu navbar-expand-lg fp ">
                 <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse"
                     data-target="#navbarNav"> ☰ </button>
-                <img src="{{ url('assets/img/CoroVid.png') }}" class="img-fluid" width="80" alt="CoroVid - Imagem">
+                <img src="assets/img/CoroVid.png" class="img-fluid" width="80" alt="CoroVid - Imagem">
                 <div class="container">
                     <div class="collapse navbar-collapse wow fadeIn" id="navbarNav">
                         <ul class="navbar-nav">
@@ -137,14 +137,14 @@
                 });
         }
     </script>
-    <script src="{{ url('assets/js/jquery.js') }}"></script>
-    <script src="{{ url('assets/js/bootstrap/bootstrap.js') }}"></script>
-    <script src="{{ url('assets/js/wow.js') }}" type="text/javascript"></script>
+    <script src="/assets/js/jquery.js"></script>
+    <script src="/assets/js/bootstrap/bootstrap.js"></script>
+    <script src="/assets/js/wow.js" type="text/javascript"></script>
     <script>
         new WOW().init();
     </script>
     <!--[SCRIPTS:Site]-->
-    <script src="{{ url(mix('assets/js/main.js')) }}"></script>
+    <script src="/assets/js/main.js"></script>
 </body>
 
 </html>
